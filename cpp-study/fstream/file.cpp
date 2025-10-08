716c309a0a84c7a3bbede22fcb5418bbee7a45ec
Ivan Kuleshov <Ivaner1234@yandex.ru>
2025-10-08 22:50:49 +0300

    played with fstream for a little

--- cpp-study/fstream/file.cpp
+++ cpp-study/fstream/file.cpp
@@ -4,7 +4,9 @@
 #include <sstream>
 
 int writeFile() {
-	std::ofstream wfile{"Sample.txt"};
+	std::ofstream wfile{"Sample.txt", std::ios::app};
+
+	// we can use wfile.open() for opening the file and wfile.close() to close the same file
 
 	if (!wfile) {
 		std::cerr << "Womp-womp, can't open the file";
@@ -13,6 +15,26 @@ int writeFile() {
 
 	wfile << "Something\n";
 	wfile << "In the way\n";
+	wfile.close();
+}
+
+int writeAnotherLinesToFile() {
+	std::ofstream outf{"Sample.txt", std::ios::app};
+
+	if (!outf) {
+		std::cerr << "We can't open this file, unlucky";
+		return 1;	
+	}
+
+	outf << "Hmmm...\n";
+	outf << "Hmmm...\n";
+	outf << std::endl;
+	outf.close();
+
+	// Appending line to the file
+	outf.open("Sample.txt", std::ios::app);
+	outf << "I'm vengeance\n";
+	outf.close();
 }
 
 int readFile() {
@@ -28,14 +50,16 @@ int readFile() {
 	// 	std::cout << strInput << '\n';
 
 	// Alternative way
-	std::stringstream ss_out;
-	while (rfile >> ss_out) 
-		std::cout << ss_out << '\n';
+	std::string strInput{};
+	while (std::getline(rfile, strInput)) 
+		std::cout << strInput << '\n';
+	rfile.close();
 }
 
 int main() {
 
-	//writeFile();
+	writeFile();
+	writeAnotherLinesToFile();
 	readFile();
 
 	return 0;
