18214682cd31898dbde1f21aab32153f80260465
Ivan Kuleshov <Ivaner1234@yandex.ru>
2026-06-03 09:32:20 +0300

    Completed the first four tasks

---
+++ python_tutorial/Introduction/second_task.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+color("blue")
+
+forward(60)
+right(60)
+forward(60)
+right(60)
+forward(60)
+right(60)
+forward(60)
+right(60)
+forward(60)
+right(60)
+forward(60)
+
+done()
---
+++ python_tutorial/Introduction/third_task.py
@@ -0,0 +1,9 @@
+from turtle import *
+
+color("red")
+angle = 5
+
+for x in range(1, 74):
+    forward(angle)
+    right(angle)
+
