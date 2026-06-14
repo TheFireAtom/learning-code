a94b67b97443c80ee42981538fca13b8f54d346d
Ivan Kuleshov <Ivaner1234@yandex.ru>
2026-06-14 15:48:39 +0300

    homework: completed

---
+++ python_tutorial/Strings/homework_task_one.py
@@ -0,0 +1,14 @@
+iword = input("Input: Введите слово: ")
+print("Output: ")
+
+print(iword[2])
+print(iword[-2])
+print(iword[0:5])
+print(iword[0:-2])
+print(iword[0::2])
+print(iword[1::2])
+print(iword[-1:0:-1])
+print(iword[-1::-2])
+print(len(iword))
+
+
