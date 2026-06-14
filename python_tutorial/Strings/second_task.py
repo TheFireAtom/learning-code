b16d1442bded32be78f92a8ff3dd35bc6b7979d3
Ivan Kuleshov <Ivaner1234@yandex.ru>
2026-06-14 09:51:16 +0300

    second task: completed

---
+++ python_tutorial/Strings/second_task.py
@@ -0,0 +1,10 @@
+word = input("Write your word: ")
+
+first_letter = word[0]
+second_letter = word[-1:]
+
+final_word = first_letter + second_letter
+
+print(first_letter)
+print(second_letter)
+print(final_word)
