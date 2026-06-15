93eae4901775b9bae74d619d8c6764e3bc104d90
Ivan Kuleshov <Ivaner1234@yandex.ru>
2026-06-15 11:07:11 +0300

    fourth task: completed

---
+++ python_tutorial/Strings Methods/fourth_task.py
@@ -0,0 +1,7 @@
+sin = input("Enter the sentence: ")
+
+sfind = input("Enter the word that you want to replace: ")
+splace = input("Enter the word that you want to place: ")
+
+sout = sin.replace(sfind, splace)
+print(sout)
